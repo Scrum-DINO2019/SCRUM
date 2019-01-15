8a64f9f1c0f147177dda40a6df6cba34e7a29f40
Alagris <alagris12358@gmail.com>
2019-01-15 19:54:02 +0100

    finished except design

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -7,9 +7,12 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 public class MainController {
@@ -21,16 +24,27 @@ public class MainController {
     private URL location;
 
     @FXML // fx:id="result"
-    private Label result; // Value injected by FXMLLoader
+    private TextArea result; // Value injected by FXMLLoader
 
     @FXML
     void keyDown(KeyEvent event) {
-    	System.out.println("eer");
+    	System.out.println(event.getCode()+" "+event.getCharacter());
+    	if(event.getCharacter().equals("w")) {
+    		exit(null);
+    	}else if(event.getCharacter().equals("p")) {
+    		next(null);
+    	}
     }
     
     @FXML
     void next(ActionEvent event) {
     	System.out.println("ee");
+    	result.setText(Math.random() > 0.5 ? "Nie ma kota" : "Jest kot");
+    }
+    @FXML
+    void exit(ActionEvent event) {
+    	Platform.exit();
+    	System.exit(0);
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
