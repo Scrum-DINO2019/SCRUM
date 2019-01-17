224673295f7b08945df69cc514d222c7b67501ee
unknown <marczakmaciej2@gmail.com>
2019-01-17 20:36:43 +0100

    message

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,19 +3,26 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-			var scene = new Scene(root,400,400);
+			
+			Parent root1 = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root1,600,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			Image anotherIcon = new Image("file:///C:/Users/barry/Downloads/SCRUM-master/src/application/cat.png");
+			primaryStage.getIcons().add(anotherIcon);
+			primaryStage.setTitle("CatRecognizer ");
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
